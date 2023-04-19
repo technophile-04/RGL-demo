85d7544a4adb2b530d17cd520596d613b326dedd
Shiv Bhonde <shivbhonde04@gmail.com>
2023-04-19 10:52:50 +0530

    setup basic react-grid-layout

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
-import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, SparklesIcon } from "@heroicons/react/24/outline";
+import { Responsive, WidthProvider } from "react-grid-layout";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
+const layout = [
+  { i: "welcome", x: 0, y: 0, w: 9, h: 3 },
+  { i: "streams", x: 9, y: 0, w: 3, h: 3 },
+  { i: "contract-info", x: 9, y: 1, w: 3, h: 1 },
+  { i: "living-room", x: 0, y: 3, w: 9, h: 1 },
+];
 
 const Home: NextPage = () => {
   return (
@@ -10,47 +18,26 @@ const Home: NextPage = () => {
         <title>Scaffold-eth App</title>
         <meta name="description" content="Created with 🏗 scaffold-eth" />
       </Head>
-
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">scaffold-eth 2</span>
-          </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold">packages/nextjs/pages/index.tsx</code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract <code className="italic bg-base-300 text-base font-bold">YourContract.sol</code> in{" "}
-            <code className="italic bg-base-300 text-base font-bold">packages/hardhat/contracts</code>
-          </p>
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <SparklesIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Experiment with{" "}
-                <Link href="/example-ui" passHref className="link">
-                  Example UI
-                </Link>{" "}
-                to build your own UI.
-              </p>
-            </div>
+      <div style={{ padding: "16px" }}>
+        <ResponsiveGridLayout
+          layouts={{ lg: layout }}
+          breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+          cols={{ lg: 12, md: 3, sm: 3, xs: 2, xxs: 1 }}
+          resizeHandles={["ne", "se", "sw", "nw"]}
+        >
+          <div className="bg-primary" key="welcome">
+            <div className="p-2">Welcome !!</div>
           </div>
-        </div>
+          <div className="bg-primary" key="streams">
+            <div className="p-2">Stream</div>
+          </div>
+          <div className="bg-primary" key="contract-info">
+            <div className="p-2">Contract balance</div>
+          </div>
+          <div className="bg-primary" key="living-room">
+            <div className="p-2">living room</div>
+          </div>
+        </ResponsiveGridLayout>
       </div>
     </>
   );
